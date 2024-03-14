75d1086b9015e43fba2648c89369ca30db79661d
Sakalis <lsverslas@gmail.com>
2024-03-14 19:54:36 +0200

    gettrip

--- src/routes/tripRoutes.ts
+++ src/routes/tripRoutes.ts
@@ -1,3 +1,12 @@
 
+import express from 'express';
+
+const tripsRouter = express.Router();
 
 // GET - /trips - textaa 'get all trips'
+tripsRouter.get('/', async (_req, res) => {
+    res.json('getting all trips');
+});
+
+export default tripsRouter;
+
