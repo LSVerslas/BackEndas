16e26c688e0ee74c078c1683af521912c9f8951a
Sakalis <lsverslas@gmail.com>
2024-03-18 10:20:42 +0200

    delete

--- src/routes/tripRoutes.ts
+++ src/routes/tripRoutes.ts
@@ -1,155 +1,134 @@
-
+// sukurti ir exportuoti routeri
 import express from 'express';
 import dbQueryWithData from '../helpers/helper.js';
 import { TripObjType } from '../helpers/types.js';
 import { ResultSetHeader } from 'mysql2';
-// import { ResultSetHeader } from 'mysql2';
 
 const tripsRouter = express.Router();
 
 // const fields = [
-// 'id', 'name', 'date', 'country', 'city', 'rating', 'description', 'price', 'user_id',
+//   'id','name','date','country','city','rating','description','price','user_id',
 // ];
-
 const tripCols = 'id,name,date,country,city,rating,description,price,user_id,image_main';
 
-// GET - /trips - textaa 'get all trips'
+// GET - /trips/ - texta 'get all trips'
 tripsRouter.get('/', async (_req, res) => {
-    const sql = `SELECT ${tripCols} FROM trips WHERE is_deleted=0`;
-    const [row, error] = (await dbQueryWithData(sql)) as [TripObjType[], Error];
+  // panaudoti dbQueryWithData
+  const sql = `SELECT ${tripCols} FROM trips WHERE is_deleted=0`;
+  const [row, error] = (await dbQueryWithData(sql)) as [TripObjType[], Error];
 
-    if (error) {
-        console.warn('get all trips error ===', error);
-        console.warn('error ===', error.message);
-        return res.status(400).json({ error: 'Something went wrong' });
-    }
+  if (error) {
+    console.warn('get all trips error ===', error);
+    console.warn('error ===', error.message);
+    return res.status(400).json({ error: 'something went wrong' });
+  }
 
-    console.log('row ===', row[0]);
+  console.log('row ===', row[0]);
 
-
-    res.json(row);
+  // gauti visus trips objektus masyvo pavidalu
+  res.json(row);
 });
 
+// - GET /trips/:id - grazinti viena irasa pagal id
 tripsRouter.get('/:tripId', async (req, res) => {
-    const currentId = req.params.tripId;
+  const currentId = req.params.tripId;
 
-    const sql = `SELECT ${tripCols} FROM trips WHERE is_deleted=0 AND id=?`;
+  const sql = `SELECT ${tripCols} FROM trips WHERE is_deleted=0 AND id=?`;
 
-    const [rows, error] = await dbQueryWithData(sql, [currentId]) as [TripObjType[], Error];
+  const [rows, error] = (await dbQueryWithData(sql, [currentId])) as [TripObjType[], Error];
 
-    if (error) {
-        console.warn('grazinti viena irasa pagal id error ===', error);
-        console.warn('error ===', error.message);
-        return res.status(400).json({ error: 'Something went wrong' });
-    }
+  if (error) {
+    console.warn('grazinti viena irasa pagal id error ===', error);
+    console.warn('error ===', error.message);
+    return res.status(400).json({ error: 'something went wrong' });
+  }
 
-    if (rows.length === 0) {
-        console.log('no rows');
-        return res.status(404).json({ msg: `trip with id: '${currentId}' was not found` });
-    }
+  // nuspresti ar radom irasa ar 404
 
-    console.log('rows ===', rows);
+  if (rows.length === 0) {
+    console.log('no rows');
+    return res.status(404).json({ msg: `trip with id: '${currentId}' was not found` });
+  }
 
-    res.json(rows[0]);
+  // console.log('rows ===', rows);
+
+  res.json(rows[0]);
 });
 
-
-
-
+// - POST /trips - sukurti nauja irasa
 tripsRouter.post('/', async (req, res) => {
+  // tai kas atsiusta gyvena ?
 
+  const {
+    name,
+    date,
+    country,
+    city,
+    rating,
+    description,
+    price,
+    user_id,
+    image_main,
+    images_1 = '',
+    images_2 = '',
+    images_3 = '',
+  } = req.body as Omit<TripObjType, 'id'>;
+  // validation
+  const argArr = [
+    name,
+    date,
+    country,
+    city,
+    rating,
+    description,
+    price,
+    user_id,
+    image_main,
+    images_1,
+    images_2,
+    images_3,
+  ];
 
-    const { 
-        name,
-        date,
-        country,
-        city,
-        rating,
-        description,
-        price,
-        user_id,
-        image_main,
-        images_1 = '',
-        images_2 = '',
-        images_3 = '',
-    } = req.body as Omit<TripObjType, 'id'>;
- 
-    const argArr = [
-        name,
-        date,
-        country,
-        city,
-        rating,
-        description,
-        price,
-        user_id,
-        image_main,
-        images_1,
-        images_2,
-        images_3,
-    ];
-    const sql = `INSERT INTO trips (name, date, country, city, rating, description, price, user_id, image_main, images_1, images_2, images_3) VALUES
-    (?,?,?,?,?,?,?,?,?,?,?,?)`;
+  // panaudoti dbQueryWithData ir sukurti nauja irasa
+  const sql = `INSERT INTO trips (name, date, country, city, rating, description, price, user_id, image_main, images_1, images_2, images_3) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`;
 
-    // let rez: ResultSetHeader
-    const[rows, error] = (await dbQueryWithData(sql, argArr)) as [ResultSetHeader, Error];
-    // rez.insertId
-    if (error) {
-        console.warn('sukurti nauja irasa error ===', error);
-        console.warn('error ===', error.message);
-        return res.status(400).json({ error: 'Something went wrong' });
-    }
+  // jei sekmingai sukurta grzinti naujai sukurto iraso id
+  const [rows, error] = (await dbQueryWithData(sql, argArr)) as [ResultSetHeader, Error];
 
+  // grazinti pilna nauja objekta
+  if (error) {
+    console.warn('sukurti nauja irasa error ===', error);
+    console.warn('error ===', error.message);
+    return res.status(400).json({ error: 'something went wrong' });
+  }
 
-
-    // res.json(rows);
-    res.json({ id: rows.insertId, ...req.body } as TripObjType);
+  // // sukurimo rezultato tipas yra ResultSetHeader
+  // let rez: ResultSetHeader;
+  // res.json(rows);
+  res.json({ id: rows.insertId, ...req.body } as TripObjType);
 });
-    // // padaryti DELETE/trip
 
+// - DELETE /trips/:id - istrinti irasa pakeiciant is_deleted i 1
 
+tripsRouter.delete('/:tripId', async (req, res) => {
+  const currentId = req.params.tripId;
 
+  const sql = `UPDATE trips SET is_deleted=1 WHERE id=? LIMIT 1`;
 
-    tripsRouter.delete('/:tripId', async (req, res) => {
-        const currentId = req.params.tripId;
+  const [rows, error] = (await dbQueryWithData(sql, [currentId])) as [ResultSetHeader, Error];
 
-        const sql = `UPDATE trip SET is_deleted=1 WHERE id=?`;
+  if (error) {
+    console.warn('istrinti irasa pakeiciant is_deleted i 1 error ===', error);
+    console.warn('error ===', error.message);
+    return res.status(400).json({ error: 'something went wrong' });
+  }
 
-        const [rows, error] = (await dbQueryWithData(sql, [currentId])) as [ResultSetHeader, Error];
+  if (rows.affectedRows === 0) {
+    console.log('no rows');
+    return res.status(404).json({ error: `trip with id: '${currentId}' was not found` });
+  }
 
-        if (error) {
-            console.warn('istrinti irasa pakeiciant is_deleted i 1 error ===', error);
-            console.warn('error ===', error.message);
-            return res.status(400).json({ error: 'Something went wrong' });
-        }
-
-        if (rows.affectedRows === 0) {
-            console.log('no rows');
-            return res.status(404).json({ msg: `trip with id: '${currentId}' was not found` });
-        }
-
-        res.json({ msg: `trip with id: '${currentId}' was deleted` });
-    });
+  res.json({ msg: `trip with id: '${currentId}' was deleted` });
+});
 
 export default tripsRouter;
-
-    // tripsRouter.delete('/:tripId', async (req, res) => {
-    //     const currentId = req.params.tripId;
-
-    //     const sql = `UPDATE trips SET is_deleted=1 WHERE id=?`;
-
-    //     const [rows, error] = await dbQueryWithData(sql, [currentId]) as [ResultSetHeader, Error];
-
-    //     if (error) {
-    //         console.warn('istrimti irasa pakeiciant is_delete i 1 error ===', error);
-    //         console.warn('error ===', error.message);
-    //         return res.status(400).json({ error: 'Something went wrong' });
-    //     }
-    // });
-
-    // padaryti UPDATE/trip
-
-    // res.json(argArr);
-
-
-
