5e39d8f6a91500b8f61111391281c224cb35ae09
Sakalis <lsverslas@gmail.com>
2024-03-19 20:34:47 +0200

    users

--- src/helpers/dbModel.sql
+++ src/helpers/dbModel.sql
@@ -43,4 +43,25 @@ INSERT INTO countries (name, description, image_main)
 VALUES 
 ('France', 'France is a country located in Western Europe. It is known for its wine, cheese, and the Eiffel Tower.', 'france.jpg'),
 ('United Kingdom', 'The United Kingdom is a country located off the northwestern coast of mainland Europe. It is made up of four countries: England, Scotland, Wales, and Northern Ireland.', 'uk.jpg'),
-('Italy', 'Italy is a country located in Southern Europe. It is known for its art, architecture, and food.', 'italy.jpg');
+('Italy', 'Italy is a country located in Southern Europe. It is known for its art, architecture, and food.', 'italy.jpg'),
+('Lithuania', 'Lithuania is a center of Europe. It is known for its beer, cepelinai, and Gediminas Tower.', 'lithuania.jpg'),
+('Jamaika', 'Jamaika is a country somewhere in the see. It is made up of islands.', 'jamaika2.jpg');
+
+
+-- Lentele users
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    is_deleted BOOLEAN NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT check_email CHECK (email LIKE '%_@__%.__%')
+);
+
+-- Vartotojai
+INSERT INTO users (username, password, email)
+VALUES
+('Francas', '1234567', 'franc@yandex.ru'),
+('Zuokas', '1234567', 'zuokas@email.mail'),
+('Budulis', '1234567', 'vagzalas@one.lt');
